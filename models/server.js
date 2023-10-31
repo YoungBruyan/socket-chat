6452d7335bc45751d824cdc6400aa5dd95604fbd
Daniel Celis <daniel.celis.alerta@gmail.com>
2023-10-30 23:33:30 -0700

    ajustando nombre de constante

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.port;
+        this.port = process.env.PORT;
 
         // Http server
         this.server = http.createServer(this.app);
