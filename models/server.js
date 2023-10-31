d9e62f239bd314cb64eb2cd6640220405ec817f0
Daniel Celis <daniel.celis.alerta@gmail.com>
2023-10-30 23:31:26 -0700

    cambios en el console.log que indicaba el puerto usado

--- models/server.js
+++ models/server.js
@@ -8,7 +8,7 @@ class Server {
 
     constructor() {
         this.app = express();
-        this.port = process.env.port || 8080;
+        this.port = process.env.port;
 
         // Http server
         this.server = http.createServer(this.app);
